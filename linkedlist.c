3d3229eee70f814b665b54b0192297eed4007fa0
Mandeep8252 <mandeepsingh.cs24@bmsce.ac.in>
2025-11-18 11:39:06 +0530

    Add files via upload

--- linkedlist.c
+++ linkedlist.c
@@ -5,6 +5,7 @@ struct node{
     struct node* next;
 };
 struct node* head;
+struct node* head2=NULL;
 void insertbeginning(){
     struct node* ptr;
     int item;
@@ -35,7 +36,7 @@ void insertlast(){
         if(head==NULL){
             ptr->next=NULL;
             head=ptr;
-            printf("Node inserted");
+            printf("Node inserted\n");
     }
         else{
             temp=head;
@@ -52,7 +53,7 @@ void insertlast(){
 void insertrandom(){
     int i, loc, item;
     struct node *ptr, *temp;
-    ptr = (struct node*)malloc(sizeof(struct node));  // Fix memory allocation
+    ptr = (struct node*)malloc(sizeof(struct node));  
     if(ptr == NULL){
         printf("Overflow\n");
     } else {
@@ -61,20 +62,20 @@ void insertrandom(){
         printf("Enter the location after which you want to add: ");
         scanf("%d", &loc);
 
-        // Check if location is valid
+        
         temp = head;
         for(i = 0; i < loc; i++) {
             temp = temp->next;
-            if(temp == NULL) {  // If we reached the end of the list before reaching 'loc'
+            if(temp == NULL) {  
                 printf("Can't insert, location is out of bounds.\n");
                 return;
             }
         }
         
-        // Now 'temp' is at the node after which we want to insert
+        
         ptr->data = item;
-        ptr->next = temp->next;  // Make new node's next point to temp's next
-        temp->next = ptr;        // Make temp's next point to the new node
+        ptr->next = temp->next;  
+        temp->next = ptr;
 
         printf("Node inserted\n");
     }
@@ -88,17 +89,164 @@ void show(){
     }
     else{
         while(ptr != NULL){
-        printf("%d",ptr->data);
+        printf("%d\t",ptr->data);
         ptr=ptr->next;
     }
 
     }
     
 }
-void main(){
+void delete(){
+    struct node *ptr;
+    ptr=head;
+    head=ptr->next;
+    free(ptr);
+    printf("Node deleted\n");
+}
+void delete_end(){
+    struct node *ptr,*temp;
+    if(head==NULL){
+        printf("List is empty\n");
+    }
+    else if(head->next==NULL){
+        
+        free(head);
+        head=NULL;
+        printf("The only node deleted\n");
+    }
+    else{
+        ptr=head;
+        while(ptr->next!=NULL){
+            temp=ptr;
+            ptr=ptr->next;
+
+        }
+        temp->next=NULL;
+        free(ptr);
+        printf("Node deleted from end\n");
+
+    }
+}
+void delete_random(){
+    struct node *ptr,*temp;
+    int loc,i;
+    printf("Enter the position at which you want to delete:");
+    scanf("%d",&loc);
+    ptr=head;
+    for(i=0;i<loc;i++){
+        temp=ptr;
+        ptr=ptr->next;
+        if(temp->next==NULL){
+            printf("Not enough elemenst in the list\n");
+            return;
+        }
+    }
+    temp->next=ptr->next;
+    free(ptr);
+    printf("Deleted at location %d",loc);
+}
+void sort(){
+    struct node *i,*j;
+    int temp;
+    for(i=head ;i!=NULL;i=i->next){
+        for(j=i->next;j!=NULL;j=j->next){
+            if(i->data>j->data){
+                temp=i->data;
+                i->data=j->data;
+                j->data=temp;
+            }
+        }
+    }
+
+}
+void insert_list_2(){
+   struct node *ptr,*temp;
+    int item;
+    ptr=(struct node*)malloc(sizeof(struct node*));
+    if(ptr==NULL){
+        printf("Overflow\n");
+    }
+    else{
+        printf("Enter the value to be added:\n");
+        scanf("%d",&item);
+        ptr->data=item;
+        if(head2==NULL){
+            ptr->next=NULL;
+            head2=ptr;
+            printf("Node inserted\n");
+    }
+        else{
+            temp=head;
+            while(temp->next !=NULL){
+                temp=temp->next;
+
+            }
+            temp->next=ptr;
+            ptr->next=NULL;
+            printf("Node inserted\n");
+    }
+}
+}
+struct node* concatenate(){
+    if(head==NULL){
+        head=head2;
+        printf("List 1 is empty");
+        return;
+    }
+    struct node *temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=head2;
+    return head;
+
+}
+void reverse(){
+    struct node *current=head,*prev=NULL,*next=NULL;
+    while(current!=NULL){
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    head=prev;
+}
+int main(){
     struct node* head=NULL;
-    insertbeginning();
-    insertlast();
-    insertrandom();
-    show();
+    int choice;
+    printf("Menu:\n");
+    printf("1.insertbeginning\n2.insertend\n3.insertrandom\n4.deletebeginning\n5.deleteend\n6.deleterandom\n7.show\n8.sort\n9.insert_list_2\n10.concatenate\n11.reverse\n12.EXIT\n");
+    
+    while(1){
+
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:insertbeginning();
+                    break;
+            case 2:insertlast();
+                    break;
+            case 3:insertrandom();
+                    break;
+            case 4:delete();
+                    break;
+            case 5:delete_end();
+                    break;
+            case 6: delete_random();
+                    break;
+            case 7:show();
+                    break;
+            case 8:sort();
+                    break;
+            case 9:insert_list_2();
+                    break;
+            case 10:concatenate();
+                    break;
+            case 11:reverse();
+                    break;
+            case 12:return 0;
+            default:
+                printf("Invaid choice.");
+        }
+    }
 }
